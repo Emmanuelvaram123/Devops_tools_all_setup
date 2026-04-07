9d84bb5c89730f272941d51972bad72d67b2b31d
Emmanuel varam <162671200+Emmanuelvaram123@users.noreply.github.com>
2026-04-07 14:03:25 +0530

    Switch from Percona to MySQL installation
    Comment out Percona installation commands and add MySQL installation steps.

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,14 @@
-wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
-sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
-percona-release setup ps80
-sudo apt install percona-server-server -y
-mysql -u root -p
+#wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+#sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+#percona-release setup ps80
+#sudo apt install percona-server-server -y
+#mysql -u root -p
+
+
+sudo rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
+sudo yum install mysql-community-server -y
+sudo systemctl enable mysqld
+sudo systemctl start mysqld
+sudo grep 'temporary password' /var/log/mysqld.log
+sudo mysql_secure_installation
